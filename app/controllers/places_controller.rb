40f8c129673409a64d1aad67774adfa57a229dc9
Evan Latham <evan.latham@kellogg.northwestern.edu>
2025-03-17 22:26:19 +0000

    test

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,7 +15,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["name"]
-    @place["user_id"] = @current_user["id"] 
+    # @place.user_id = @current_user.id
     @place.save
     redirect_to "/places"
   end

403c28e09fc76f10606e1a0683cfae84c8ad75c5
Evan Latham <evan.latham@kellogg.northwestern.edu>
2025-03-17 17:50:24 +0000

    css styling done

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
     t.date "occurred_on"
     t.integer "place_id"
     t.integer "user_id"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

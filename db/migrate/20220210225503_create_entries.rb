075aebf2c46cb9a4d4d19bf36ea92f9dddde43be
Evan Latham <evan.latham@kellogg.northwestern.edu>
2025-03-18 03:36:29 +0000

    image attribute in entries table migration

--- db/migrate/20220210225503_create_entries.rb
+++ db/migrate/20220210225503_create_entries.rb
@@ -6,6 +6,7 @@ class CreateEntries < ActiveRecord::Migration[7.0]
       t.date "occurred_on"
       t.integer "place_id"
       t.integer "user_id"
+      t.string "image"
 
       t.timestamps
     end
